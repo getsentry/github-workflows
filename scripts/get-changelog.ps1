9d701562ce7be8c9158c5cfc75794ea0224cd65f
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-07-06 19:42:25 +0200

    feat: add changelog to the PR

---
+++ scripts/get-changelog.ps1
@@ -0,0 +1,64 @@
+param(
+    [Parameter(Mandatory = $true)][string] $RepoUrl,
+    [Parameter(Mandatory = $true)][string] $OldTag,
+    [Parameter(Mandatory = $true)][string] $NewTag
+)
+
+Set-StrictMode -Version latest
+
+$prefix = '^https://(www\.)?github.com/'
+if (-not ($RepoUrl -match $prefix)) {
+    Write-Warning "Only github.com repositories are currently supported. Given RepoUrl doesn't look like one: $RepoUrl"
+    return
+}
+
+$tmpDir = Join-Path ([System.IO.Path]::GetTempPath()) ([System.Guid]::NewGuid())
+
+try
+{
+    git clone --depth 1 $RepoUrl $tmpDir
+
+    $file = $(Get-ChildItem -Path $tmpDir | Where-Object { $_.Name -match '^changelog(\.md|\.txt|)$' } )
+    if ("$file" -eq "")
+    {
+        Write-Warning "Couldn't find a changelog"
+        return
+    }
+    elseif ($file -is [Array])
+    {
+        Write-Warning "Multiple changelogs found: $file"
+        return
+    }
+    Write-Host "Found changelog: $file"
+    [string[]]$lines = Get-Content $file
+}
+finally
+{
+    Write-Host "Removing $tmpDir"
+    Remove-Item -Recurse -Force -ErrorAction Continue -Path $tmpDir
+}
+
+$foundFirst = $false
+$changelog = ""
+for ($i = 0; $i -lt $lines.Count; $i++)
+{
+    $line = $lines[$i]
+
+    if (-not $foundFirst) {
+        if ($line -match "^#+ +v?$NewTag\b") {
+            $foundFirst = $true
+        } else {
+            continue
+        }
+    } elseif ($line -match "^#+ +v?$OldTag\b") {
+        break
+    }
+
+    $changelog += "$line`n"
+}
+
+$changelog = $changelog.Trim()
+if ($changelog.Length -gt 1) {
+    $changelog = "## Changelog`n" + $($changelog -replace "^#","##")
+}
+$changelog
---
+++ scripts/set-github-env.ps1
@@ -0,0 +1,12 @@
+param(
+    [Parameter(Mandatory = $true)][string] $Name,
+    [Parameter(Mandatory = $true)][string] $Data
+)
+
+Set-StrictMode -Version latest
+
+if ($null -eq $env:GITHUB_ENV) {
+    throw "GITHUB_ENV environment variable is missing - this script is supposed to be run in GitHub-Actions."
+}
+
+Write-Output "$Name<<EOF`n$Data`nEOF" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
