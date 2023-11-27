ee0c9cc246b91e2fb77a868ea892367344d11d1a
Ivan Dlugos <dlugos.ivan@gmail.com>
2023-11-27 12:52:19 +0100

    test: git@ repos

--- updater/tests/nonbot-commits.ps1
+++ updater/tests/nonbot-commits.ps1
@@ -2,32 +2,34 @@ Set-StrictMode -Version latest
 
 . "$PSScriptRoot/common/test-utils.ps1"
 
-function NonBotCommits([Parameter(Mandatory = $true)][string] $branch)
+foreach ($repoUrl in @('https://github.com/getsentry/github-workflows', 'git@github.com:getsentry/github-workflows.git'))
 {
-    $result = & "$PSScriptRoot/../scripts/nonbot-commits.ps1" `
-        -RepoUrl 'https://github.com/getsentry/github-workflows' -MainBranch 'main' -PrBranch $branch
-    if (-not $?)
+    function NonBotCommits([Parameter(Mandatory = $true)][string] $branch)
     {
-        throw $result
+        $result = & "$PSScriptRoot/../scripts/nonbot-commits.ps1" -RepoUrl $repoUrl -MainBranch 'main' -PrBranch $branch
+        if (-not $?)
+        {
+            throw $result
+        }
+        elseif ($LASTEXITCODE -ne 0)
+        {
+            throw "Script finished with exit code $LASTEXITCODE"
+        }
+        $result
     }
-    elseif ($LASTEXITCODE -ne 0)
-    {
-        throw "Script finished with exit code $LASTEXITCODE"
+
+    RunTest 'empty-if-all-commits-by-bot' {
+        $commits = NonBotCommits 'deps/updater/tests/sentry-cli.properties'
+        AssertEqual '' "$commits"
     }
-    $result
-}
 
-RunTest 'empty-if-all-commits-by-bot' {
-    $commits = NonBotCommits 'deps/updater/tests/sentry-cli.properties'
-    AssertEqual '' "$commits"
-}
+    RunTest 'empty-if-branch-doesnt-exist' {
+        $commits = NonBotCommits 'non-existent-branch'
+        AssertEqual '' "$commits"
+    }
 
-RunTest 'empty-if-branch-doesnt-exist' {
-    $commits = NonBotCommits 'non-existent-branch'
-    AssertEqual '' "$commits"
-}
-
-RunTest 'non-empty-if-changed' {
-    $commits = NonBotCommits 'test/nonbot-commits'
-    AssertEqual '0b7d9cc test: keep this branch' "$commits"
+    RunTest 'non-empty-if-changed' {
+        $commits = NonBotCommits 'test/nonbot-commits'
+        AssertEqual '0b7d9cc test: keep this branch' "$commits"
+    }
 }
