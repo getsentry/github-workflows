03ac81adcd6ded1cec60c9096de85796721be2f1
Ivan Dlugos <dlugos.ivan@gmail.com>
2025-01-24 09:58:10 +0100

    feat: support proper sorting for prereleases

--- updater/scripts/sort-versions.ps1
+++ updater/scripts/sort-versions.ps1
@@ -4,37 +4,14 @@ param(
 
 Set-StrictMode -Version latest
 
-function GetComparablePart([Parameter(Mandatory = $true)][string] $value)
-{
-    $value.PadLeft(10, '0')
-}
-
 function GetComparableVersion([Parameter(Mandatory = $true)][string] $value)
 {
     $value = $value -replace '^v', ''
-    $output = ''
-    $buffer = ''
-    for ($i = 0; $i -lt $value.Length; $i++)
-    {
-        $char = $value[$i]
-        if ("$char" -match '[^a-zA-Z0-9]')
-        {
-            # Found a separtor, update the current buffer
-            $output += GetComparablePart $buffer
-            $output += $char
-            $buffer = ''
-        }
-        else
-        {
-            $buffer += $char
-        }
+    try {
+        [System.Management.Automation.SemanticVersion]::Parse($value)
+    } catch {
+        Write-Error "Failed to parse semantic version '$value': $_"
     }
-    if ($buffer.Length -gt 0)
-    {
-        $output += GetComparablePart $buffer
-    }
-
-    $output
 }
 
 $List | Sort-Object -Property @{Expression = { GetComparableVersion $_ } }
