2b043dff4557d1f131657fa49b1734e437c3d752
Ivan Dlugos <6349682+vaind@users.noreply.github.com>
2025-06-20 23:05:22 +0200

    Update get-changelog.Tests.ps1
    This got broken by [an unlucky change](https://github.com/getsentry/sentry-native/commit/4ebdbe900a085c0a97f8ef52edeeef04bf715da6#diff-06572a96a58dc510037d5efa622f9bec8519bc1beab13c9f251e97e657a9d4edR43) in an old changelog item we rely on in our tests...

--- updater/tests/get-changelog.Tests.ps1
+++ updater/tests/get-changelog.Tests.ps1
@@ -134,7 +134,7 @@ Features, fixes and improvements in this release have been contributed by:
 
 - Add support for Xbox Series X/S. ([#1100](https://github-redirect.dependabot.com/getsentry/sentry-native/pull/1100))
 - Add option to set debug log level. ([#1107](https://github-redirect.dependabot.com/getsentry/sentry-native/pull/1107))
-- Add `traces_sampler` ([#1108](https://github-redirect.dependabot.com/getsentry/sentry-native/pull/1108))
+- Add `traces_sampler`. ([#1108](https://github-redirect.dependabot.com/getsentry/sentry-native/pull/1108))
 - Provide support for C++17 compilers when using the `crashpad` backend. ([#1110](https://github-redirect.dependabot.com/getsentry/sentry-native/pull/1110), [crashpad#116](https://github-redirect.dependabot.com/getsentry/crashpad/pull/116), [mini_chromium#1](https://github-redirect.dependabot.com/getsentry/mini_chromium/pull/1))
 '@
 
